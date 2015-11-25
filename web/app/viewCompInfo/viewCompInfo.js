e99e4921e2d84c545060179fd18fd44c66d3b120
favl <favl.dk@gmail.com>
2015-11-25 14:25:02 +0100

    backend for companyInfo
    provided the code for the GET method for a company search.

--- web/app/viewCompInfo/viewCompInfo.js
+++ web/app/viewCompInfo/viewCompInfo.js
@@ -3,20 +3,30 @@
 angular.module('myApp.viewCompInfo', ['ngRoute'])
 
         .config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/viewCompInfo', {
-              templateUrl: 'app/viewCompInfo/viewCompInfo.html',
-              controller: 'viewCompInfoCtrl'
-            });
-          }])
+                $routeProvider.when('/viewCompInfo', {
+                    templateUrl: 'app/viewCompInfo/viewCompInfo.html',
+                    controller: 'viewCompInfoCtrl'
+                });
+            }])
 
         .controller('viewCompInfoCtrl', function ($http, $scope) {
-          $http({
-            method: 'GET',
-            url: 'api/demouser'
-          }).then(function successCallback(res) {
-            $scope.data = res.data.message;
-          }, function errorCallback(res) {
-            $scope.error = res.status + ": "+ res.data.statusText;
-          });
+            $http({
+                method: 'GET',
+                url: 'api/demouser'
+            }).then(function successCallback(res) {
+                $scope.data = res.data.message;
+            }, function errorCallback(res) {
+                $scope.error = res.status + ": " + res.data.statusText;
+            });
 
+            $scope.cvrapi = function () {
+                $http.get('http://cvrapi.dk/api?search=' + $scope.searchInput + '&country=' + $scope.searchCountry)
+                        .success(function (response) {
+                            $scope.company = response;
+                        })
+                        .error(function (response) {
+                            console.log(response);
+                            $scope.apiError = response;
+                        });
+            };
         });
