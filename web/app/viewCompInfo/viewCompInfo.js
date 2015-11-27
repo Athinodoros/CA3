b853415f981c5f5b410488557266e0d1ad17e30c
fandul <rihards.pacevics@gmail.com>
2015-11-27 13:45:55 +0100

    some styling

--- web/app/viewCompInfo/viewCompInfo.js
+++ web/app/viewCompInfo/viewCompInfo.js
@@ -10,17 +10,23 @@ angular.module('myApp.viewCompInfo', ['ngRoute'])
             }])
 
         .controller('viewCompInfoCtrl', function ($http, $scope, DisableAuthInterceptor) {
-
+            $scope.er = false;
+            $scope.apiError = "Such company does not exist ";
             $scope.cvrapi = function () {
                 DisableAuthInterceptor.enableLoader = false;
                 $http.get('http://cvrapi.dk/api?search=' + $scope.searchInput + '&country=' + $scope.searchCountry)
                         .success(function (response) {
+                            $scope.er = false;
                             $scope.company = response;
+                            if ($scope.company.error == "NOT_FOUND") {
+                                $scope.er = true;
+                            }
+                            $scope.error = response.error;
                             DisableAuthInterceptor.enableLoader = true;
                         })
                         .error(function (response) {
                             console.log(response);
-                            $scope.apiError = response;
+                            $scope.er = true;
                             DisableAuthInterceptor.enableLoader = true;
                         });
             };
